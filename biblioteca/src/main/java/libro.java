93bc5475b4d69395f9978580f282d2b652921325
Yeferal <yefercuber20@gmail.com>
2019-07-27 01:18:53 -0600

    se agrega la classe libro

---
+++ biblioteca/src/main/java/libro.java
@@ -0,0 +1,38 @@
+
+/**
+ *
+ * @author yefer
+ */
+public class libro {
+    
+    String titulo;
+    String autor;
+    String codigo;
+    int cantidad;
+    String fechaPublicacion;
+    String editorial;
+    
+    public libro(String titulo, String autor, String codigo, int cantidad){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.codigo=codigo;
+        this.cantidad=cantidad;
+    }
+    
+    public String getTitulo(){
+        return titulo;
+    }
+    
+    public String getAutor(){
+        return autor;
+    }
+    
+    public String getCodigo(){
+        return codigo;
+    }
+    
+    public int getCantidad(){
+        return cantidad;
+    }
+    
+}
